7be5d0d546d3787012f125e86da0e526aa4ece51
Himay <himayradec@gmail.com>
2022-04-21 11:26:09 -0700

    Player Movement Without Animation Implemeted

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -21,9 +21,13 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // xRotation is the current axis of the mouse
         xRotation -= mouseY;
+
+        // limits the value of xRotation in between the range of -90 and 90
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        // rotates the camera using the current xRotation value
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }

da710c45e2c8264c01b555a34d494004188e9097
Himay <himayradec@gmail.com>
2022-05-02 16:55:32 -0700

    Worked On WaveSystem.cs Enemy.cs Spawner.cs

--- Assets/UI_KC/Kanosh_Prefabs/Scripts/Spawner.cs
+++ Assets/UI_KC/Kanosh_Prefabs/Scripts/Spawner.cs
@@ -4,63 +4,48 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    private int waveCount = 0;
-    private int enemySpawnCount = 0;
-    private int enemiesKilled = 0;
 
-    public GameObject[] spawners;
-    public GameObject enemy;
-
-    private void Start()
-    {
-        spawners = new GameObject[5];
-
-        for (int i = 0; i < spawners.Length; i++)
-        {
-            spawners[i] = transform.GetChild(i).gameObject; // GETCHILD check the unity doc
-        }
-
-        StartWave();
-    }
+    public float spawnCooldown = 5f;
+    public bool isEnabled = false;
 
     private void Update()
     {
-        if (enemySpawnCount < 5)
+        // If enemy is withing radius
+        // enable object
+        // if not disable object
+        if (isEnabled)
         {
-            SpawnEnemy();   
+            SpawnCooldownTimer();
 
+            if (spawnCooldown == 0)
+            {
+                SpawnEnemy();
+
+            }
         }
-            
+
     }
 
     private void SpawnEnemy()
     {
-        int spawnerID = Random.Range(0, spawners.Length);
-        Instantiate(enemy, spawners[spawnerID].transform.position, spawners[spawnerID].transform.rotation);
-        enemySpawnCount++;
-    }
-
-    private void StartWave()
-    {
-        waveCount = 1;
-        enemySpawnCount = 2;
-        enemiesKilled = 0;
-
-        for (int i = 0; i < enemySpawnCount; i++)
+        if (WaveSystem.CanSpawnEnemies())
         {
-            SpawnEnemy();
+            // Spawn an Enemy
+            // create game object of the enemy at the current spawner location
+
+            WaveSystem.currentSpawnedEnemies++;
+            WaveSystem.totalSpawnedEnemies++;
+
+            spawnCooldown = 5f;
         }
     }
 
-    private void NextWave()
+    private void SpawnCooldownTimer()
     {
-        waveCount++;
-        enemySpawnCount += 2;
-        enemiesKilled = 0;
-
-        for (int i = 0; i < enemySpawnCount; i++)
+        if (spawnCooldown > 0)
         {
-            SpawnEnemy();
+            spawnCooldown = spawnCooldown - 1 * Time.deltaTime;
         }
+        
     }
 }
